da42ab88985306c49ecb2a33bb44997b9bd60ff2
Lyle <740334078@qq.com>
2023-09-13 18:28:07 +0800

    ✨ feat: 美化滚动条

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,11 @@
 const path = require('path')
-
+const CracoLessPlugin = require('craco-less')
 module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+    },
+  ],
   webpack: {
     alias: {
       '@': path.resolve(__dirname, './src'),
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { memo } from 'react'
+
+const App = memo(() => {
+  return (
+    <div>
+      <div style={{ height: '100vh' }}>123</div>
+    </div>
+  )
+})
+
+export default App
